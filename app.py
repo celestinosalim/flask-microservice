15338be14b7ebf51d675f3a1a736c147041fc8ed
celestinosalim <celestino.salim@gmail.com>
2019-06-17 14:50:03 -0400

    added env variable

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ application_limits=["5 per minute"])
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lnujflspcwrqms:565ca04b0f1bb7c7153eb44e8872e89172801ab9185cd99ace4dd7135f35c119@ec2-107-20-230-70.compute-1.amazonaws.com:5432/d56amubtoat4hh'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE')
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
